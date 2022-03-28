30d9ab4ccadd48dbbe10297fc6fa536b1554ae00
JSSatorres <juansataz@gmail.com>
2022-03-28 14:17:44 +0200

    controller function

--- backend/controller/productsControllers.js
+++ backend/controller/productsControllers.js
@@ -11,13 +11,12 @@ export const getProducts = async (req, res) => {
 };
 
 export const getProduct = async (req, res) => {
-
-  // return res.json(newProducts);
   try {
-    const { title, description } = req.body;
-    const newProducts = new Products({ title, description });
-    await newProducts.save();
-    res.json(newProducts)
+    const oneProduct = await Products.findById(req.params.id);
+    if(!oneProduct){
+      return res.send("product nof found by id")
+    }
+    res.json(oneProduct);
   } catch (error) {
     next(error);
   }
@@ -28,19 +27,34 @@ export const createtProducts = async (req, res) => {
     const { title, description } = req.body;
     const newProducts = new Products({ title, description });
     await newProducts.save();
-    // res.json({"newProducts":"sfdsfs"})
-    res.json(newProducts)
-    console.log(req.body);
-
+    res.json(newProducts);
   } catch (error) {
     next(error);
   }
 };
 
 export const deleteProducts = async (req, res) => {
-  res.send("delete porducts");
+  try {
+    const producDeleted = await Products.findByIdAndRemove(req.params.id);
+    if (!producDeleted) {
+      return res.send("product not found");
+    }
+    return res.send("Product Deleted");
+  } catch (error) {
+    next(error);
+  }
 };
 
 export const updateProducts = async (req, res) => {
-  res.send("delete porducts");
+  try {
+    const { productId } = req.params.id;
+    const productUpdate = await Products.findByIdAndUpdate(
+      productId,
+      req.body,
+      { new: true }
+    );
+    return res.send("update Product");
+  } catch (error) {
+    next(error);
+  }
 };
